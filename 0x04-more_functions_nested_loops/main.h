d874595975868ab45f45d252221b743aa08f7973
Oluwafemi1444 <oberazaq2@gmail.com>
2022-12-15 08:14:11 -0800

    Rrmoved

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,10 +6,7 @@
  * Header file that contains the declearation of more functions
  */
 
-int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
 
 #endif

fc3a86fe6aa4e39dc1d9107b1a1837774a395d94
Oluwafemi1444 <oberazaq2@gmail.com>
2022-12-16 12:30:01 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,9 +5,15 @@
  * main.h file
  * Header file that contains the declearation of more functions
  */
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 #endif
